903fea95c6008bc5b3ca1741fb2c392eaa5115cd
DragonFruitName <revathyshree464@gmail.com>
2025-06-16 17:15:37 +0530

    create banner

---
+++ src/Components/Banner.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import {Container, Row, Col} from "react-bootstrap";
+import {ArrowRightCircle} from "react-bootstrap-icons";
+import bannerimg1 from "../assets/img/banner-img1.jpg";
+import bannerimg2 from "../assets/img/banner-img2.jpg";
+import bannerimg3 from "../assets/img/banner-img3.jpg";
+
+function Banner(){
+
+    //Background effects:
+    const bgImages = [bannerimg1, bannerimg2, bannerimg3];
+    const textColor=['#FFB823' , '#FFF1CA', '#AF3E3E'];
+    const [currentBgIndex, setCurrentBgIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+        setCurrentBgIndex((prevIndex) => (prevIndex + 1) % bgImages.length);
+        }, 8000); 
+
+        return () => clearInterval(interval); 
+    }, [bgImages.length]);
+    
+
+    return (
+        <section>
+
+            <Container 
+                className={"banner"}
+                id="home" 
+                style={{
+                    backgroundImage: `url(${bgImages[currentBgIndex]})`,
+                    }}
+            >
+                <Row class="align-items-center">
+
+                    <Col xs={12} md={6} xl={7}>
+                    <span className="tagline">Welcome to my portfolio!</span>
+                    <h1 style={{color:textColor[currentBgIndex]}}>I'm a Web Developer</h1>
+                    <p>
+                        I'm a web developer who loves creating responsive, 
+                        full-stack applications. I'm currently working on several projects 
+                        to learn and explore real-world implementation of the MERN stack 
+                        (MongoDB, Express, React, Node.js).
+                    </p>
+                    <button onClick={()=>{console.log('button got clicked')}}>Let's connect<ArrowRightCircle size={25}/></button>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
+
+export default Banner;
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,10 @@
+import {useState, useEffect} from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {useState, useEffect} from "react";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 
 function NavBar() {
     //to address the location of the user
@@ -53,9 +52,8 @@ function NavBar() {
 
         <span className='navbar-text'>
             <div className='social-icon'>
-                <a href="#"> <img src={navIcon1} alt="" /></a>
-                <a href="#"> <img src={navIcon2} alt="" /></a>
-                <a href="#"> <img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/revathi-v-971374231/"> <img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/DragonFruitCookie"> <img src={navIcon2} alt="" /></a>
             </div>
             <button className='vvd' onClick={()=> console.log('got clicked')}><span>Let's connect!</span></button>
         </span>
